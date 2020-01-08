e8f2cc4937458887a1171c7fc35e5c139aac168a
jyoti-maurya <mauryajyoti76@gmail.com>
2020-01-08 16:29:07 +0530

    final commit with proper file

--- src/components/CalculateWinner.js
+++ src/components/CalculateWinner.js
@@ -1,6 +1,4 @@
 export const CalculateWinner = (squares) => {
-  console.log('cal calculateWinner');
-  console.log(squares);
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -14,11 +12,8 @@ export const CalculateWinner = (squares) => {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      console.log(lines[i]);
-      // console.log(squares[lines[i][0]]);
       return lines[i];
     }
   }
-  console.log('helllo ');
   return null;
 }
